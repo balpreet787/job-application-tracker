61e2ae15135e2f29b962f5637879dc2e32d86948
Erryl <errylgalang@gmail.com>
2024-09-07 21:50:51 -0700

    Adjust header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,7 +2,7 @@ import {CircleUserRound} from "lucide-react";
 
 export default function Header() {
     return (
-      <header className="flex justify-between w-full p-2 border-b border-slate-200 font-bold items-center">
+      <header className="flex justify-between w-full py-2 px-4 border-b border-slate-200 font-bold items-center">
         <h1 className="text-2xl">Coop Dashboard</h1>
         <CircleUserRound />
       </header>

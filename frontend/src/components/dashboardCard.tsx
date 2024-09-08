4550c2604ab412cb36180425f312be669e4386c4
Erryl <errylgalang@gmail.com>
2024-09-07 21:24:16 -0700

    Create dashboardCard

---
+++ frontend/src/components/dashboardCard.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+interface DashboardCardProps {
+  title: string
+  value: string | number
+  icon: React.ElementType
+}
+
+export default function DashboardCard({ title, value, icon: Icon }: DashboardCardProps) {
+  return (
+    <Card className="basis-1/4">
+      <CardHeader className="flex flex-row items-center pb-8">
+        <Icon className="size-6"/>
+        <span className="font-semibold text-xl pb-2 pl-2">{title}</span>
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">{value}</div>
+      </CardContent>
+    </Card>
+  )
+}
